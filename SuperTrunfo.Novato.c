6c8d17ed6c421a1d918cb65a9ac3e5a4e718080f
mendesmarcelo <159094591+mendesmarcelo@users.noreply.github.com>
2025-02-26 01:25:11 +0000

    carta 01 quase completa

--- SuperTrunfo.Novato.c
+++ SuperTrunfo.Novato.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
 int main(){
-printf("Desafio Super Trunfo\n"); 
+printf("***Desafio Super Trunfo***\n"); 
+
+char estado;
+char codigo;
+char cidade;
+int populacao;
+float area;
+float PIB;
+int pontos;
+
+printf("Digite o estado: \n");
+scanf("%s", &estado);
+
+printf("Digite o código da carta: \n");
+scanf("%s", &codigo);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &cidade);
+
+printf(" Digite a população: \n");
+scanf("%d", &populacao);
+
+printf("Digite a área em km2:\n");
+scanf("%f, &area);
+
+    printf("Digite o PIB:\n");
+scanf("%f", &PIB);
+
+printf("Digite a quantidade de pontos turísticos:\n");
+scanf("%d", &pontos);
+
+
 
 
 
