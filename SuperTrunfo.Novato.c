f83982971577ee5f61618863f7846160b8bb9e55
mendesmarcelo <159094591+mendesmarcelo@users.noreply.github.com>
2025-02-27 00:22:42 +0000

    carta 01 "pronta"

--- SuperTrunfo.Novato.c
+++ SuperTrunfo.Novato.c
@@ -3,38 +3,43 @@
 int main(){
 printf("***Desafio Super Trunfo***\n"); 
 
-char estado;
-char codigo;
-char cidade;
-int populacao;
+char estado[50];
+char codigo[50];
+char cidade[50];
+float populacao;
 float area;
-float PIB;
+float pib;
 int pontos;
 
 printf("Digite o estado: \n");
-scanf("%s", &estado);
+scanf("%s", estado);
 
 printf("Digite o código da carta: \n");
-scanf("%s", &codigo);
+scanf("%s", codigo);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", &cidade);
+scanf("%s", cidade);
 
 printf(" Digite a população: \n");
-scanf("%d", &populacao);
+scanf("%f", &populacao);
 
 printf("Digite a área em km2:\n");
 scanf("%f", &area);
   printf("Digite o PIB:\n");
-scanf("%f", &PIB);
+scanf("%f", &pib);
 
 printf("Digite a quantidade de pontos turísticos:\n");
 scanf("%d", &pontos);
 
-printf("Nome do Estado: %s\n");
+
+    printf("Nome do Estado: %s\n", estado);
+    printf("Código da Carta: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.f km2\n", area);
+    printf("PIB: %.f\n", pib);
+    printf("Pontos Turísticos: %d\n", pontos);
 
 
 
 return 0;
-
-}
